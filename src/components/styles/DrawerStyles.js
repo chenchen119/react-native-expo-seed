acb1c3903e33d36d1e40832598f7a9c1533403d3
Chen Li <l1chen87@gmail.com>
2017-05-16 18:46:43 -0400

    initial commits

---
+++ src/components/styles/DrawerStyles.js
@@ -0,0 +1,29 @@
+//import { Metrics, Colors, Fonts } from '../../themes'
+
+export default {
+  container: {
+    flex: 1,
+    backgroundColor: '#191919'
+  },
+  withIcon: {
+      flexDirection: 'row',
+      alignItems: 'center'
+  },
+  iconWithText: {
+      marginRight: 10,
+      paddingLeft: 20
+  },
+  text: {
+      color: '#b3b3b3',
+      fontSize: 15
+  },
+  textWithIcon: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingTop: 40,
+      paddingVertical: 15,
+      borderColor: '#000',
+      borderBottomWidth: 3
+  },
+}
