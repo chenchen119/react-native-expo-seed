30f837624ade05a535bb7fe1c2d9d5f3b570bcd2
Chen Li <l1chen87@gmail.com>
2017-05-16 19:19:08 -0400

    initial commits

--- src/containers/HomeScreen.js
+++ src/containers/HomeScreen.js
@@ -1,4 +1,3 @@
-import { Video } from 'expo';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
 var { height, width } = Dimensions.get('window');
